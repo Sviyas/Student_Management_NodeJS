67feda2a3343f35b695172c31bc1e23d366b4f11
Viyas <viyas@tealvue.com>
2022-12-28 18:07:48 +0530

    update : add custom error message on validator

--- validator/index.js
+++ validator/index.js
@@ -5,10 +5,17 @@ const baseValidator = async (req, res, next, schema) => {
     console.log('schema identifer', value);
     next();
   } catch (err) {
-    return res.status(403).json({
-      result: false,
-      message: `${err.message}  Validator length must be valid`
-    });
+    const errror = JSON.parse(JSON.stringify(err.message));
+    // console.log('🚀 ~ file: index.js:9 ~ baseValidator ~ errror', errror);
+
+    if (errror) {
+      const message = `please provide a correct credintials`;
+
+      return res.status(403).json({
+        result: false,
+        message: message
+      });
+    }
   }
 };
 
